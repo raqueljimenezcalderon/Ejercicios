877252f693a8fe6f3f514dc6b2efd7d00e5bae26
raqueljimenezcalderon <32922181+raqueljimenezcalderon@users.noreply.github.com>
2019-09-24 08:57:36 +0200

    Añadido un mensaje de Hola mundo

--- Ejemplo/src/Main.java
+++ Ejemplo/src/Main.java
@@ -31,7 +31,7 @@ public class Main extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		setTitle("Mi nueva ventana");
+		setTitle("Hola mundo");
 	}
 
 }
